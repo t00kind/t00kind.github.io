5a22aa261b286628d0bbfdbe8897ff20450a68ec
poshlit <68813582+poshlit@users.noreply.github.com>
2021-06-07 16:18:56 +0500

    Update main.js

--- happy_birthday/main.js
+++ happy_birthday/main.js
@@ -1,6 +1,13 @@
 new Vue({
   el: "#vue",
   data: {
-    name: "Ильшат"
-  }
+    name: "Ильшат",
+    show: true,
+    real: false;
+  },
+  methods: {
+    re: function() {
+          this.show = false;
+          this.real = true;
+    }
 })
